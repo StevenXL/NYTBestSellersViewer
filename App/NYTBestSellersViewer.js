5e18ff88e8480112e0feea31737c496a893534b7
Steven Leiva <leiva.steven@gmail.com>
2016-08-16 23:13:52 -0400

    Ensure linter passes.
    - Configured "react/jsx-filename-extension" rule to allow .js extension.

--- App/NYTBestSellersViewer.js
+++ App/NYTBestSellersViewer.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -25,21 +24,18 @@ const styles = StyleSheet.create({
   },
 });
 
-export default class NYTBestSellersViewer extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+const NYTBestSellersViewer = () =>
+  <View style={styles.container}>
+    <Text style={styles.welcome}>
+      Welcome to React Native!
+    </Text>
+    <Text style={styles.instructions}>
+      To get started, edit index.ios.js
+    </Text>
+    <Text style={styles.instructions}>
+      Press Cmd+R to reload,{'\n'}
+      Cmd+D or shake for dev menu
+    </Text>
+  </View>;
+
+export default NYTBestSellersViewer;
