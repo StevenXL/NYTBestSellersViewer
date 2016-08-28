66ca540b8f12fdad170ad68d4f7e74536cb70ed4
Steven Leiva <leiva.steven@gmail.com>
2016-08-28 15:55:03 -0400

    Refactor Category component.

--- js/views/CategoriesView/Category.js
+++ js/views/CategoriesView/Category.js
@@ -3,25 +3,15 @@ import { View, Text } from 'react-native';
 
 const Category = ({
   displayName,
-  listName,
-  listNameEncoded,
 }) =>
   <View>
     <Text>
       {displayName}{'\n'}
     </Text>
-    <Text>
-      {listName}{'\n'}
-    </Text>
-    <Text>
-      {listNameEncoded}{'\n'}
-    </Text>
   </View>;
 
 Category.propTypes = {
   displayName: React.PropTypes.string.isRequired,
-  listName: React.PropTypes.string.isRequired,
-  listNameEncoded: React.PropTypes.string.isRequired,
 };
 
 export default Category;
