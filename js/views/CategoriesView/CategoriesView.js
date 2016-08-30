6f34d8e71ed1889eb8ab87395b2e2cb639aa2f68
Steven Leiva <leiva.steven@gmail.com>
2016-08-29 21:56:40 -0400

    Pass `ownProps` to `mapStateToProps` and utilize `mapDispatchToProps`.
    - The `ownProps` argument to `mapStateToProps` allows me to pass down
      props from the Container component to the single Presentational
    component that it renders.
    - `mapDispatchToProps` allows me to define props that will have access
      to `store.dispatch`, allow those functions to dispatch actions.

--- js/views/CategoriesView/CategoriesView.js
+++ js/views/CategoriesView/CategoriesView.js
@@ -1,6 +1,12 @@
 import { connect } from 'react-redux';
 import CategoryList from './CategoryList';
+import requestCategory from '../../actions/requestCategory';
 
-const mapStateToProps = (state) => ({ categories: state.categories });
+const mapStateToProps = (state, ownProps) =>
+  ({ categories: state.categories, navigator: ownProps.navigator });
 
-export default connect(mapStateToProps)(CategoryList);
+const mapDispatchToProps = (dispatch) => ({
+  onCategoryClick: (listNameEncoded) => dispatch(requestCategory(listNameEncoded)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
