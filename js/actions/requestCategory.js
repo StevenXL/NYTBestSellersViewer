a8f694752e44dcf393e39dc6df768ab51b476b54
Steven Leiva <leiva.steven@gmail.com>
2016-08-31 22:31:42 -0400

    Add moment dependency to project.
    - Utilize moment to request today's data in requestCategory action.

--- js/actions/requestCategory.js
+++ js/actions/requestCategory.js
@@ -1,8 +1,10 @@
 import categoryFetched from './categoryFetched';
+import moment from 'moment';
 
 const requestCategory = (listNameEncoded) =>
   (dispatch) => {
-    fetch(`https://api.nytimes.com/svc/books/v3/lists/2016-08-01/${listNameEncoded}.json?api-key=25bbed0c29de425885f377d561c79bf7`)
+    const date = moment().format('YYYY-MM-DD').toString();
+    fetch(`https://api.nytimes.com/svc/books/v3/lists/${date}/${listNameEncoded}.json?api-key=25bbed0c29de425885f377d561c79bf7`)
       .then((response) => response.json())
       .then((responseJson) => {
         dispatch(categoryFetched(listNameEncoded, responseJson));
